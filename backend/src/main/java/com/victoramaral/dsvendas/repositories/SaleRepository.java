fc78c0ac36de5e3cd203eb9c8871a0addbcf4913
Victor Amaral <tito.victor@hotmail.com>
2021-05-05 22:19:13 -0300

    Pagination

---
+++ backend/src/main/java/com/victoramaral/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.victoramaral.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.victoramaral.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
