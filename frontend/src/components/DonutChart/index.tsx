c471bf25f15b79b746c19379d6d8c03078ab46c9
Victor Amaral <tito.victor@hotmail.com>
2021-05-07 11:22:34 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,16 +11,20 @@ type ChartDate = {
 
 const DonutChart = () => {
     
-    let chartDate: ChartDate = { labels: [], series: []};
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+    const [chartDate, setChartDate] = useState<ChartDate>({ labels: [], series: []});
+
+    useEffect(() =>{
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
         .then((response) => {
             const data =  response.data as SaleSum[];
             const myLabels = data.map(x => x.sellerName);
             const mySeries = data.map(x => x.sum);
 
-            chartDate = { labels: myLabels, series: mySeries};
-            console.log(chartDate);
-        })
+            setChartDate({ labels: myLabels, series: mySeries});
+            
+        });
+    }, []);
+    
    // const mockData = {
    //     series: [477138, 499928, 444867, 220426, 473088],
    //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
